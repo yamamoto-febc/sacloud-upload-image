c9000286bf68d34172f6a83e20d496e643eac6bd
febc.yamamoto <yamamoto.febc@gmail.com>
2016-05-05 21:08:21 +0900

    generate codes by gcli

---
+++ cli.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+)
+
+// Exit codes are int values that represent an exit code for a particular error.
+const (
+	ExitCodeOK    int = 0
+	ExitCodeError int = 1 + iota
+)
+
+// CLI is the command line object
+type CLI struct {
+	// outStream and errStream are the stdout and stderr
+	// to write message from the CLI.
+	outStream, errStream io.Writer
+}
+
+// Run invokes the CLI with the given arguments.
+func (cli *CLI) Run(args []string) int {
+	var (
+		token  string
+		secret string
+		zone   string
+
+		version bool
+	)
+
+	// Define option flag parse
+	flags := flag.NewFlagSet(Name, flag.ContinueOnError)
+	flags.SetOutput(cli.errStream)
+
+	flags.StringVar(&token, "token", "", "AccessToken")
+	flags.StringVar(&token, "t", "", "AccessToken(Short)")
+
+	flags.StringVar(&secret, "secret", "", "AccessTokenSecret")
+	flags.StringVar(&secret, "s", "", "AccessTokenSecret(Short)")
+
+	flags.StringVar(&zone, "zone", "", "Target zone")
+	flags.StringVar(&zone, "z", "", "Target zone(Short)")
+
+	flags.BoolVar(&version, "version", false, "Print version information and quit.")
+
+	// Parse commandline flag
+	if err := flags.Parse(args[1:]); err != nil {
+		return ExitCodeError
+	}
+
+	// Show version
+	if version {
+		fmt.Fprintf(cli.errStream, "%s version %s\n", Name, Version)
+		return ExitCodeOK
+	}
+
+	_ = token
+
+	_ = secret
+
+	_ = zone
+
+	return ExitCodeOK
+}
---
+++ cli_test.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func TestRun_versionFlag(t *testing.T) {
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	cli := &CLI{outStream: outStream, errStream: errStream}
+	args := strings.Split("./sacloud-upload-image -version", " ")
+
+	status := cli.Run(args)
+	if status != ExitCodeOK {
+		t.Errorf("expected %d to eq %d", status, ExitCodeOK)
+	}
+
+	expected := fmt.Sprintf("sacloud-upload-image version %s", Version)
+	if !strings.Contains(errStream.String(), expected) {
+		t.Errorf("expected %q to eq %q", errStream.String(), expected)
+	}
+}
+
+func TestRun_tokenFlag(t *testing.T) {
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	cli := &CLI{outStream: outStream, errStream: errStream}
+	args := strings.Split("./sacloud-upload-image -token", " ")
+
+	status := cli.Run(args)
+	_ = status
+}
+
+func TestRun_secretFlag(t *testing.T) {
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	cli := &CLI{outStream: outStream, errStream: errStream}
+	args := strings.Split("./sacloud-upload-image -secret", " ")
+
+	status := cli.Run(args)
+	_ = status
+}
+
+func TestRun_zoneFlag(t *testing.T) {
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	cli := &CLI{outStream: outStream, errStream: errStream}
+	args := strings.Split("./sacloud-upload-image -zone", " ")
+
+	status := cli.Run(args)
+	_ = status
+}
